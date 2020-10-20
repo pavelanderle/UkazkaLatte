ffaa11b6f24f66a32add049da48382de9782eb7e
Pavel <anderle@spseplyen.cz>
2020-10-20 10:28:29 +0200

    upgrade template1.latte

--- temp/template1.latte--1e29236e9b.php
+++ temp/template1.latte--1e29236e9b.php
@@ -5,6 +5,14 @@ use Latte\Runtime as LR;
 
 class Template1e29236e9b extends Latte\Runtime\Template
 {
+	public $blocks = [
+		'sidebar' => 'blockSidebar',
+	];
+
+	public $blockTypes = [
+		'sidebar' => 'html',
+	];
+
 
 	function main()
 	{
@@ -33,7 +41,7 @@ class Template1e29236e9b extends Latte\Runtime\Template
 		}
 ?>
 
-    <h2>Výpis pole hodnot pomoc9 n:atribut;</h2>
+    <h2>Výpis pole hodnot pomocí n:atribut;</h2>
 <?php
 		$iterations = 0;
 		foreach ($items1 as $item) {
@@ -43,7 +51,7 @@ class Template1e29236e9b extends Latte\Runtime\Template
 		}
 ?>
 
-    <h2>Použití podmínek - </h2>
+    <h2>Použití podmínek - výpis hodnoty "two" barevně</h2>
 <?php
 		$iterations = 0;
 		foreach ($items1 as $item) {
@@ -59,8 +67,41 @@ class Template1e29236e9b extends Latte\Runtime\Template
 		}
 ?>
 
+    <h2>Podmínky</h2>
+    <p>Úkol: Vypište dle hodnoty proměnné $ovoce alkoholický nápoj, který se z něj vyrábí (jablko,švestka,hruška)</p>
+<?php
+		if ($ovoce="jablko") {
+?>
+    <p>vypálíme kalvados</p>
+<?php
+		}
+		elseif ($ovoce="švestky") {
+?>
+    <p>vypálíme slivovici</p>
+<?php
+		}
+?>
+
+    <h2>Ternární operátor</h2>
+    <p>Úkol: Vypište dle hodnoty proměnné $rezervace větu Rezervováno nebo Volné k pronájmu</p>
+    <p>dostupnost: <?php echo LR\Filters::escapeHtmlText($rezervace ? 'Rezervováno' : 'Dostupné k pronájmu') /* line 41 */ ?></p>
+
+    <h2>Cyklus Foreach</h2>
+    <p>Úkol: Vypište pole $objednávka jako HTML seznam</p>
+
+    <h2>Cyklus For</h2>
+    <p>Úkol: Vypište první tři položky pole $objednávka jako HTML seznam (formát výpisu - 1. položka 2.položka atd.)</p>
     
-    
+    <h2>Bloky</h2>
+    <p>Úkol: Vytvořte blok HTML kódu a použijte na jiném místě šablony</p>
+<?php
+		if ($this->getParentName()) return get_defined_vars();
+		$this->renderBlock('sidebar', get_defined_vars());
+?>
+
+<?php
+		$this->renderBlock('sidebar', get_defined_vars(), 'html');
+?>
 </body>
 </html>
 
@@ -75,6 +116,18 @@ class Template1e29236e9b extends Latte\Runtime\Template
 		extract($this->params);
 		if (isset($this->params['item'])) trigger_error('Variable $item overwritten in foreach on line 15, 20, 23');
 		
+	}
+
+
+	function blockSidebar($_args)
+	{
+?>	<h3>Menu</h3>
+	<ul>
+        <li>Položka 1</li>
+        <li>Položka 2</li>
+        <li>Položka 3</li>
+    </ul>
+<?php
 	}
 
 }

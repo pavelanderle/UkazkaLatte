1ae4557dbd4835e2496f21463e21bebae1fc8992
pavelanderle <anderle@spseplzen.cz>
2020-10-22 12:53:47 +0200

    Update calorie.php

--- calorie.php
+++ calorie.php
@@ -1,1 +1,21 @@
-//todo
+<?php
+    require_once("src/latte.php");
+    $latte = new Latte\Engine;
+
+    $latte->setTempDirectory('temp');
+
+    /* hodnoty z formuláře */
+    $stupen = $_GET["stupen"];
+    $pocetPiv = $_GET["pocetpiv"];
+    
+    /* vypocty */
+    // todo
+    
+    /* vypoctene hodnoty */
+    $params = [
+        
+    ];
+    
+    // kresli na výstup
+    $latte->render('template/calTemplate.latte', $params);
+    ?>
